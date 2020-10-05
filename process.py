0c43f476bac0bdc71d63936f99fbf5641864dcd7
Shrubabati777 <63135977+Shrubabati777@users.noreply.github.com>
2020-10-05 20:34:57 +0530

    Update process.py

--- process.py
+++ process.py
@@ -1,19 +1,18 @@
 import cv2
 
 
-# Read the image file
 image = cv2.imread('images (2).jpg')
-# Convert to Grayscale Image
+# Converting to grayscale
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#Canny Edge Detection
+# Edge detection
 canny_edge = cv2.Canny(gray_image, 170, 200)
 
-# Find contours based on Edges
+# Finding contours
 contours, new  = cv2.findContours(canny_edge.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 contours=sorted(contours, key = cv2.contourArea, reverse = True)[:30]
 
-# Initialize license Plate contour and x,y coordinates
+
 contour_with_license_plate = None
 license_plate = None
 x = None
@@ -21,7 +20,7 @@ y = None
 w = None
 h = None
 
-# Find the contour with 4 potential corners and creat ROI around it
+# Extraction of the plate only
 for contour in contours:
         # Find Perimeter of contour and it should be a closed contour
         perimeter = cv2.arcLength(contour, True)
@@ -32,11 +31,11 @@ for contour in contours:
             license_plate = gray_image[y:y + h, x:x + w]
             break
 
-# Removing Noise from the detected image, before sending to Tesseract
+# Noise removal
 license_plate = cv2.bilateralFilter(license_plate, 11, 17, 17)
 (thresh, license_plate) = cv2.threshold(license_plate, 150, 180, cv2.THRESH_BINARY)
 
 
-
+# Display
 cv2.imshow("License Plate Detection",license_plate)
 cv2.waitKey(0)
