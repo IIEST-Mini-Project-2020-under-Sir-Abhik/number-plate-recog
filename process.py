17ef06cf7f04be2c41b2958aee858a493c90e9d4
the-distortion <soham86das@gmail.com>
2020-10-06 20:45:40 +0530

    User now enters the image name; the canny threshold has been modified to include a larger range

--- process.py
+++ process.py
@@ -1,17 +1,21 @@
 import cv2
 
 
-image = cv2.imread('images (2).jpg')
+image = cv2.imread(input('Enter the full name/path of the car image: '))
 # Converting to grayscale
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Edge detection
-canny_edge = cv2.Canny(gray_image, 170, 200)
+canny_edge = cv2.Canny(gray_image, 120, 200)
 
 # Finding contours
 contours, new  = cv2.findContours(canny_edge.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 contours=sorted(contours, key = cv2.contourArea, reverse = True)[:30]
 
+#for debugging contours
+# imgcpy = image.copy()
+# cv2.drawContours(imgcpy, contours, -1, (0,255,0), 1)
+# cv2.imshow('contours', imgcpy)
 
 contour_with_license_plate = None
 license_plate = None
@@ -28,6 +32,7 @@ for contour in contours:
         if len(approx) == 4: #see whether it is a Rect
             contour_with_license_plate = approx
             x, y, w, h = cv2.boundingRect(contour)
+            if w/h < 1: continue #as the aspect ratio of the number plate will be wider
             license_plate = gray_image[y:y + h, x:x + w]
             break
 
@@ -35,7 +40,8 @@ for contour in contours:
 license_plate = cv2.bilateralFilter(license_plate, 11, 17, 17)
 (thresh, license_plate) = cv2.threshold(license_plate, 150, 180, cv2.THRESH_BINARY)
 
+shape = license_plate.shape #dimensions of the license plate
 
 # Display
-cv2.imshow("License Plate Detection",license_plate)
+cv2.imshow("License Plate Detection", cv2.resize(license_plate, (100*shape[1]//shape[0], 100)))
 cv2.waitKey(0)
